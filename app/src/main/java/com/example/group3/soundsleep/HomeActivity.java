1eeb5234433ce72e33fa6bc65a04d91b24550ec4
escottmurrell <escottmurrell@hntb.com>
2018-11-23 18:09:46 -0500

    starting theme color selection

--- app/src/main/java/com/example/group3/soundsleep/HomeActivity.java
+++ app/src/main/java/com/example/group3/soundsleep/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.group3.soundsleep;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public void toSettings(View view)
+    {
+        Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/group3/soundsleep/SettingsActivity.java
+++ app/src/main/java/com/example/group3/soundsleep/SettingsActivity.java
@@ -1,4 +1,37 @@
 package com.example.group3.soundsleep;
 
-public class SettingsActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class SettingsActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
+        setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.button1:
+                Utils.changeToTheme(this, Utils.THEME_DEFAULT);
+                break;
+            case R.id.button2:
+                Utils.changeToTheme(this, Utils.THEME_DARK);
+                break;
+            case R.id.button3:
+                Utils.changeToTheme(this, Utils.THEME_BLUE);
+                break;
+        }
+    }
+
 }
