f40e0b6dec1fde261f39ece1acf9b7c2f51b09a4
escottmurrell <escottmurrell@hntb.com>
2018-11-28 10:48:59 -0500

    working on theme color selection

--- app/src/main/java/com/example/group3/soundsleep/SettingsActivity.java
+++ app/src/main/java/com/example/group3/soundsleep/SettingsActivity.java
@@ -4,23 +4,45 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SettingsActivity extends Activity implements OnClickListener {
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.RED;
+
+public class SettingsActivity extends Activity  {
+    private Activity act;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        act = this;
         super.onCreate(savedInstanceState);
         Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_settings);
-
-        findViewById(R.id.button1).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
-        findViewById(R.id.button3).setOnClickListener(this);
+        button1 = findViewById(R.id.button1);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                button1.setTextColor(RED);
+//                Utils.changeToTheme(act, Utils.THEME_DEFAULT);
+//            }
+//        });
+//        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Utils.changeToTheme(act, Utils.THEME_DARK);
+//            }
+//        });
+//        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Utils.changeToTheme(act, Utils.THEME_DEFAULT);
+//            }
+//        });
     }
 
-    @Override
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
+//    @Override
+    public void newOnClick(View v) {
         switch (v.getId()) {
             case R.id.button1:
                 Utils.changeToTheme(this, Utils.THEME_DEFAULT);
